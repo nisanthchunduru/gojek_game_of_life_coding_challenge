a3e51a0467831df64d74912dde4352d1ff45fd7c
Nisanth Chunduru <nisanth074@gmail.com>
2016-03-20 03:27:05 +0530

    An alive cell dies if it has 5, 6, 7 or 8 neighbours

--- spec/game_of_life_spec.rb
+++ spec/game_of_life_spec.rb
@@ -88,6 +88,80 @@ BOARD
           expect(game_of_life.run).to eq(board_after_one_iteration)
         end
       end
+
+      context "the cell has five alive neighbours" do
+        it "dies" do
+          board = <<BOARD
+AAD
+AAA
+DAD
+BOARD
+          board_after_one_iteration = <<BOARD
+ADA
+DDA
+AAA
+BOARD
+          game_of_life = GameOfLife.new(board)
+          expect(game_of_life.run).to eq(board_after_one_iteration)
+        end
+      end
+
+      context "the cell has six alive neighbours" do
+        it "dies" do
+          board = <<BOARD
+AAA
+AAA
+DAD
+BOARD
+          board_after_one_iteration = <<BOARD
+ADA
+DDD
+AAA
+BOARD
+          game_of_life = GameOfLife.new(board)
+          expect(game_of_life.run).to eq(board_after_one_iteration)
+        end
+
+        it "uses an infinite grid"
+      end
+
+      context "the cell has seven alive neighbours" do
+        it "dies" do
+          board = <<BOARD
+AAA
+AAA
+AAD
+BOARD
+          board_after_one_iteration = <<BOARD
+ADA
+DDD
+ADA
+BOARD
+          game_of_life = GameOfLife.new(board)
+          expect(game_of_life.run).to eq(board_after_one_iteration)
+        end
+
+        it "uses an infinite grid"
+      end
+
+      context "the cell has eight alive neighbours" do
+        it "dies" do
+          board = <<BOARD
+AAA
+AAA
+AAA
+BOARD
+          board_after_one_iteration = <<BOARD
+ADA
+DDD
+ADA
+BOARD
+          game_of_life = GameOfLife.new(board)
+          expect(game_of_life.run).to eq(board_after_one_iteration)
+        end
+
+        it "uses an infinite grid"
+      end
     end
   end
 end
