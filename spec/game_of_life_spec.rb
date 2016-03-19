e1632f46d9a7755cb50bd974098483e01a713bf5
Nisanth Chunduru <nisanth074@gmail.com>
2016-03-20 01:51:11 +0530

    An alive cell with one alive neighbour dies

--- spec/game_of_life_spec.rb
+++ spec/game_of_life_spec.rb
@@ -20,6 +20,23 @@ BOARD
           expect(game_of_life.run).to eq(board_after_one_iteration)
         end
       end
+
+      context "the cell has one alive neighbour" do
+        it "dies" do
+          board = <<BOARD
+DDD
+DAA
+DDD
+BOARD
+          board_after_one_iteration = <<BOARD
+DDD
+DDD
+DDD
+BOARD
+          game_of_life = GameOfLife.new(board)
+          expect(game_of_life.run).to eq(board_after_one_iteration)
+        end
+      end
     end
   end
 end
